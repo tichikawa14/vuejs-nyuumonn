0cf7a5b63a9b432f4b2632901a5daef36b778a05
tichikawa14 <gakusei.umeeee@gmail.com>
2020-01-06 10:59:05 +0900

    6章完

---
+++ vuejs-nyuumonn/6/bar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bar">
+    <h1 class="header">Barコンポーネント</h1>
+    <p>これはFooコンポーネントです</p>
+    <foo/>
+  </div>
+</template>
+<script>
+  import Foo from './foo'
+  // import Bar from './bar'
+  export default {
+    components: { Foo }
+  }
+</script>
+
+
