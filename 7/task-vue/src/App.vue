5bf933b04a704500ba0d92feaeef0e97cd8b7a9d
tichikawa14 <gakusei.umeeee@gmail.com>
2020-01-16 15:40:32 +0900

    ディレクトリ構成変更

--- 7/task-vue/src/App.vue
+++ 7/task-vue/src/App.vue
@@ -1,21 +1,28 @@
 <template>
-  <div>
-    <h2>タスク一覧</h2>
-    <ul>
-      <li v-for="task in tasks" v-bind:key="task.id">
-        <input type="checkbox" v-bind:checked="task.done">
-        {{ task.name }}
-      </li>
-    </ul>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-  export default {
-    computed: {
-      tasks() {
-        return this.$store.state.tasks // ストアを読む
-      },
-    },
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
   }
+}
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
